801b67877d4cbb837f077686facad3222062f290
rizkinasutionleader <118278626+rizkinasutionleader@users.noreply.github.com>
2022-11-20 18:03:36 +0700

    update cors socket

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.use((req, res) => {
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: "*",
     credentials: true
   },
 });
